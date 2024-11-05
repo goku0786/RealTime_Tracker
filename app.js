8fa7fe3d1ef95af44e14391cb45c8f27c7d3fab2
goku0786 <gk1237860@gmail.com>
2024-11-05 13:45:16 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.get('/', function (req, res) {
     res.render("index");
 });
 
-server.listen(3000, function () {
-    console.log('listening on port 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, function () {
+    console.log(`Server is running on port ${PORT}`);
 });
